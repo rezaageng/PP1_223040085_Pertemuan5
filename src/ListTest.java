6e30f42128359980d69200c519bbdd096b5e4ad5
rezaa <rezaageng5@gmail.com>
2024-03-10 22:20:10 +0700

    feat: add pertemuan 5

---
+++ src/ListTest.java
@@ -0,0 +1,17 @@
+public class ListTest {
+    public static void main(String[] args) {
+        ListStructure list = new ListStructure();
+
+        list.addHead(2);
+        list.addHead(9);
+        list.addHead(7);
+
+        list.displayElements();
+
+        System.out.println("\n# remove head");
+
+        list.removeHead();
+
+        list.displayElements();
+    }
+}
